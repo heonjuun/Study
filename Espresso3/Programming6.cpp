6a4bb21c1deecf3b4e52906d3ae992603eb9477e
heonjunkim <s01077314486@gmail.com>
2024-09-29 23:20:13 +0900

    7번문제

--- Espresso3/Programming6.cpp
+++ Espresso3/Programming6.cpp
@@ -1,49 +1,49 @@
-#include<iostream>
-using namespace std;
-
-int array_equal(int a[], int b[], int size);
-
-int array_equal(int a[], int b[], int size)
-{
-	for (int i = 0; i < size; i++)
-	{
-		if (a[i] != b[i]) {
-			return 0;
-		}
-	}
-	return 1;
-}
-
-
-int main()
-{
-	const int size = 5;
-	int a[size] = { 0 ,};
-	int b[size] = { 0 ,};
-	
-	cout << "a원소 입력:";
-	for (int i = 0; i < size; i++)
-	{
-		cin >> a[i];
-	}
-	
-	cout << "b원소 입력:";
-	for (int i = 0; i < size; i++)
-	{
-		cin >> b[i];
-	}
-	if (array_equal(a, b, size))
-	{
-		cout << "same" << endl;
-	}
-	else {
-		cout << "not same" << endl;
-	}
-	
-
-
-	
-		
-
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//int array_equal(int a[], int b[], int size);
+//
+//int array_equal(int a[], int b[], int size)
+//{
+//	for (int i = 0; i < size; i++)
+//	{
+//		if (a[i] != b[i]) {
+//			return 0;
+//		}
+//	}
+//	return 1;
+//}
+//
+//
+//int main()
+//{
+//	const int size = 5;
+//	int a[size] = { 0 ,};
+//	int b[size] = { 0 ,};
+//	
+//	cout << "a원소 입력:";
+//	for (int i = 0; i < size; i++)
+//	{
+//		cin >> a[i];
+//	}
+//	
+//	cout << "b원소 입력:";
+//	for (int i = 0; i < size; i++)
+//	{
+//		cin >> b[i];
+//	}
+//	if (array_equal(a, b, size))
+//	{
+//		cout << "same" << endl;
+//	}
+//	else {
+//		cout << "not same" << endl;
+//	}
+//	
+//
+//
+//	
+//		
+//
+//	return 0;
+//}
---
+++ Espresso3/Programming7.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace std;
+	char x[3][3] = { {' ',' ',' '},{' ',' ',' '},{' ',' ',' '} };
+void print_array()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << "|" << x[i][j];
+		}
+		cout << endl;
+	}
+}
+
+bool win()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		if (x[i][0] != ' ' &&
+			x[i][0] == x[i][1] &&
+			x[i][1] == x[i][2]) {
+			return true;
+		}
+	}
+	for (int i = 0; i < 3; i++) {
+		if (x[0][i] != ' ' &&
+			x[0][i] == x[1][i] &&
+			x[1][i] == x[2][i]) {
+
+			return true;
+		}
+	}
+	if (x[0][0] != ' ' &&
+		x[0][0] == x[1][1] && x[1][1] == x[2][2]) {
+		return true;
+	}
+	if (x[0][2] != ' ' &&
+		x[0][2] == x[1][1] && x[1][1] == x[2][0]) {
+		return true;
+	}
+	return false;
+
+}
+
+
+
+int main()
+{
+	int location_x;
+	int location_y;
+	int ping_pong=0;
+	print_array();
+	for (int i = 0; i < 9; i++) {
+		cout << "x ÁÂÇ¥ ÀÔ·Â: " << endl;
+		cin >> location_x;
+		cout << "y ÁÂÇ¥ ÀÔ·Â: " << endl;
+		cin >> location_y;
+
+		if (ping_pong == 0) {
+			x[location_x][location_y] = 'O';
+			ping_pong = 1;
+			print_array();
+			if (win() == true) {
+				cout << "O °¡ ÀÌ±è" << endl;
+				exit(1);
+			}
+		}
+		else {
+			x[location_x][location_y] = 'X';
+			ping_pong = 0;
+			print_array();
+			if (win() == true) {
+				cout << "X °¡ ÀÌ±è" << endl;
+				exit(1);
+			}
+		}
+	}
+	cout << "¹«½ÂºÎÀÔ´Ï´Ù." << endl;
+
+	
+
+
+
+	return 0;
+}
