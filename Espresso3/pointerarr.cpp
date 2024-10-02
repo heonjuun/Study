06a2a28b72cc35974d1a7cc56374834d409eb9ba
heonjunkim <s01077314486@gmail.com>
2024-10-02 23:42:44 +0900

    포인터배열

---
+++ Espresso3/pointerarr.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+	int arr[3] = { 10, 20, 30 };
+	int* pt_arr = arr;
+
+	std::cout << "배열 이름으로 배열의 요소 출력 : " << arr[0] << ", " << arr[1] << ", " << arr[2] << std::endl;
+	std::cout << "포인터로 배열의 요소 출력: " << pt_arr[0] << ", " << pt_arr[1] << ", " << pt_arr[2] << std::endl;
+	std::cout << "배열 이름에 정수를 더해서 출력" << *(arr) << ", " << *(arr + 1) << ", " << *(arr + 2) << std::endl;
+	return 0;
+}
