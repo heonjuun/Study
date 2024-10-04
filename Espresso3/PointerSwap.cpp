65dfb7c75186a036b844cd828533775c559fe1cf
heonjunkim <s01077314486@gmail.com>
2024-10-04 23:30:51 +0900

    problem8

--- Espresso3/PointerSwap.cpp
+++ Espresso3/PointerSwap.cpp
@@ -1,40 +1,40 @@
-#include<iostream>
-using namespace std;
-
-void SwapIC(int num1, int num2)
-{
-	int temp = num1;
-	num1 = num2;
-	num2 = temp;
-}
-void Swap1(int* num1, int* num2)
-{
-	int temp = *num1;
-	*num1 = *num2;
-	*num2 = temp;
-}
-void Swap2(int& num1, int& num2)
-{
-	int temp = num1;
-	num1 = num2;
-	num2 = temp;
-
-}
-int main()
-{
-	int p1 = 20;
-	int p2 = 5;
-
-	SwapIC(p1, p2);
-	std::cout << "p1의 값은?: " << p1 << std::endl;
-	std::cout << "p2의 값은?: " << p2 << std::endl;
-
-	Swap1(&p1, &p2); // 주소에 의한 호출
-	std::cout << "p1의 값은?: " << p1 << std::endl;
-	std::cout << "p2의 값은?: " << p2 << std::endl;
-
-	Swap2(p1, p2); // 참조에 의한 호출
-	std::cout << "p1의 값은?: " << p1 << std::endl;
-	std::cout << "p2의 값은?: " << p2 << std::endl;
-
-}
+//#include<iostream>
+//using namespace std;
+//
+//void SwapIC(int num1, int num2)
+//{
+//	int temp = num1;
+//	num1 = num2;
+//	num2 = temp;
+//}
+//void Swap1(int* num1, int* num2)
+//{
+//	int temp = *num1;
+//	*num1 = *num2;
+//	*num2 = temp;
+//}
+//void Swap2(int& num1, int& num2)
+//{
+//	int temp = num1;
+//	num1 = num2;
+//	num2 = temp;
+//
+//}
+//int main()
+//{
+//	int p1 = 20;
+//	int p2 = 5;
+//
+//	SwapIC(p1, p2);
+//	std::cout << "p1의 값은?: " << p1 << std::endl;
+//	std::cout << "p2의 값은?: " << p2 << std::endl;
+//
+//	Swap1(&p1, &p2); // 주소에 의한 호출
+//	std::cout << "p1의 값은?: " << p1 << std::endl;
+//	std::cout << "p2의 값은?: " << p2 << std::endl;
+//
+//	Swap2(p1, p2); // 참조에 의한 호출
+//	std::cout << "p1의 값은?: " << p1 << std::endl;
+//	std::cout << "p2의 값은?: " << p2 << std::endl;
+//
+//}
--- Espresso3/Programming8.cpp
+++ Espresso3/Programming8.cpp
@@ -4,4 +4,76 @@
 //int main()
 //{
 //
+//    int subject[5][3] = { 0 };
+//
+//    for (int i = 0; i < 5; i++)
+//    {
+//        cout << "학번" << i + 1 << "점수입력: " << endl;
+//        for (int j = 0; j < 3; j++)
+//        {
+//            cout << "과목" << j + 1 << ":";
+//            cin >> subject[i][j];
+//        }
+//    }
+//    cout << "\n점수목록" << endl;
+//
+//    for (int i = 0; i < 5; i++)
+//    {
+//        cout << "학번:" << i + 1 << ":";
+//
+//        for (int j = 0; j < 3; j++)
+//        {
+//            cout << subject[i][j] << " ";
+//
+//        }
+//        cout << endl;
+//    }
+//    for (int j = 0; j < 3; j++)
+//    {
+//        int max = subject[0][j];
+//        int min = subject[0][j];
+//        int sum = 0;
+//        for (int i = 0; i < 5; i++)
+//        {
+//            if (subject[i][j] > max)
+//                max = subject[i][j];
+//            if (subject[i][j] < min)
+//                min = subject[i][j];
+//            sum += subject[i][j];
+//        }
+//        double avg = static_cast<double>(sum) / 5;
+//        cout << "과목 " << j + 1 << "의 최고 점수: " << max << endl;
+//        cout << "과목 " << j + 1 << "의 최저 점수: " << min<< endl;
+//        cout << "과목 " << j + 1 << "의 평균 점수: " << avg<< endl;
+//        cout << "---------------------------------" << endl;
+//
+//    }
+//
+//    for (int i = 0; i < 5; i++) // 각 학생에 대해
+//    {
+//        int maxScore = subject[i][0];
+//        int minScore = subject[i][0];
+//        int sumScore = 0;
+//
+//        for (int j = 0; j < 3; j++) // 3개의 과목 점수 확인
+//        {
+//            if (subject[i][j] > maxScore)
+//                maxScore = subject[i][j];
+//            if (subject[i][j] < minScore)
+//                minScore = subject[i][j];
+//            sumScore += subject[i][j];
+//        }
+//
+//        double avgScore = static_cast<double>(sumScore) / 3; // 평균 계산
+//
+//        cout << "학번 " << i + 1 << "의 최고 점수: " << maxScore << endl;
+//        cout << "학번 " << i + 1 << "의 최저 점수: " << minScore << endl;
+//        cout << "학번 " << i + 1 << "의 평균 점수: " << avgScore << endl;
+//        cout << "---------------------------------" << endl;
+//    }
+//
+//
+//
 //}
+//
+//
