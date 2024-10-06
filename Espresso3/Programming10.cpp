88a7482379688f8df7011c62c14e37561e8574f1
heonjunkim <s01077314486@gmail.com>
2024-10-06 23:40:59 +0900

    h

--- Espresso3/Programming10.cpp
+++ Espresso3/Programming10.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
-using namespace std;
-
-int* search(int* a, int n, int x)
-{
-
-}
-int main()
-{
-
-
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//int* search(int* a, int n, int x)
+//{
+//
+//}
+//int main()
+//{
+//
+//
+//	return 0;
+//}
