416b069d16dcc7570d4c4228fd34a37d65451e71
heonjunkim <s01077314486@gmail.com>
2024-10-08 23:27:33 +0900

    functionpointer

--- Espresso3/function.cpp
+++ Espresso3/function.cpp
@@ -1,21 +1,21 @@
-#include <functional>
-#include <iostream>
-
-int foo()
-{
-    return 5;
-}
-
-int goo()
-{
-    return 6;
-}
-
-int main()
-{
-    std::function<int()> fcnPtr; // int를 반환하고 매개 변수가 없는 함수 포인터 변수 fctPtr 선언
-    fcnPtr = goo; // fcnPtr은 함수 goo를 가리킨다.
-    std::cout << fcnPtr(); // 일반적인 함수처럼 함수를 호출할 수 있다.
-
-    return 0;
-}
+//#include <functional>
+//#include <iostream>
+//
+//int foo()
+//{
+//    return 5;
+//}
+//
+//int goo()
+//{
+//    return 6;
+//}
+//
+//int main()
+//{
+//    std::function<int()> fcnPtr; // int를 반환하고 매개 변수가 없는 함수 포인터 변수 fctPtr 선언
+//    fcnPtr = goo; // fcnPtr은 함수 goo를 가리킨다.
+//    std::cout << fcnPtr(); // 일반적인 함수처럼 함수를 호출할 수 있다.
+//
+//    return 0;
+//}
