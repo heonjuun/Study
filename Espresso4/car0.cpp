868a2abc7091646ba189bc46873bdbbcc19b77e3
heonjunkim <s01077314486@gmail.com>
2024-10-15 23:23:40 +0900

    class

---
+++ Espresso4/car0.cpp
@@ -0,0 +1,30 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//class Car {
+//public:
+//	int speed;
+//	int gear;
+//	string color;
+//
+//	void speedUp()
+//	{
+//		speed += 10;
+//	}
+//	void speedDown()
+//	{
+//		speed -= 10;
+//	}
+//};
+//int main()
+//{
+//	Car mycar;
+//	mycar.speed = 100;
+//	mycar.gear = 3;
+//	mycar.color = "red";
+//	mycar.speedUp();
+//	mycar.speedDown();
+//
+//	return 0;
+//}
