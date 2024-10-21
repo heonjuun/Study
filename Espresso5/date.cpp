7975285a70160fff5405de0758ad458718f9819d
heonjunkim <s01077314486@gmail.com>
2024-10-21 22:04:01 +0900

    car3

--- Espresso5/date.cpp
+++ Espresso5/date.cpp
@@ -1,19 +1,19 @@
-#include<iostream>
-#include<string>
-using namespace std;
-class Date {
-public:
-	void printDate()
-	{
-		cout << year << "." << month << "." << day << endl;
-	}
-	int getDay()
-	{
-		return day;
-	}
-
-	int year;
-	string month;
-	int day;
-
-};
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//class Date {
+//public:
+//	void printDate()
+//	{
+//		cout << year << "." << month << "." << day << endl;
+//	}
+//	int getDay()
+//	{
+//		return day;
+//	}
+//
+//	int year;
+//	string month;
+//	int day;
+//
+//};
---
+++ Espresso5/employee.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//class Employee {
+//private: //접근 제한
+//	string name;
+//	int salary;
+//	int age;
+//
+//	int getSalary() { return salary; }
+//public:
+//	int getAge() { return age; }
+//	string getName() { return name; }
+//};
+//
+//int main()
+//{
+//	Employee e;
+//    string s = e.getName();
+//	int a = e.getAge();		
+//	return 0;
+//}
