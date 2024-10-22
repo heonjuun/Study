3a760c3a0b1f5d00806a988123913fd44ed5de6f
heonjunkim <s01077314486@gmail.com>
2024-10-22 21:34:04 +0900

    car4

--- Espresso5/car3.cpp
+++ Espresso5/car3.cpp
@@ -1,49 +1,49 @@
-#include<iostream>
-#include<string>
-using namespace std;
-
-class Car {
-private:
-	int speed;
-	int gear;
-	string color;
-public:
-	int getSpeed()
-	{
-		return speed;
-	}
-	void setSpeed(int s)
-	{
-		speed = s;
-	}
-	int getGear()
-	{
-		return gear;
-	}
-	void setGear(int g)
-	{
-		gear = g;
-	}
-	string getColor()
-	{
-		return color;
-	}
-	void setColor(string c)
-	{
-		color = c;
-	}
-
-};
-
-int main()
-{
-	Car myCar;
-	myCar.setSpeed(100);
-	myCar.setGear(3);
-	myCar.setColor("red");
-
-	cout << "차의 속도:" << myCar.getSpeed() << endl;
-	cout << "차의 기어:" << myCar.getGear() << endl;
-	cout << "차의 색상:" << myCar.getColor() << endl;
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//class Car {
+//private:
+//	int speed;
+//	int gear;
+//	string color;
+//public:
+//	int getSpeed()
+//	{
+//		return speed;
+//	}
+//	void setSpeed(int s)
+//	{
+//		speed = s;
+//	}
+//	int getGear()
+//	{
+//		return gear;
+//	}
+//	void setGear(int g)
+//	{
+//		gear = g;
+//	}
+//	string getColor()
+//	{
+//		return color;
+//	}
+//	void setColor(string c)
+//	{
+//		color = c;
+//	}
+//
+//};
+//
+//int main()
+//{
+//	Car myCar;
+//	myCar.setSpeed(100);
+//	myCar.setGear(3);
+//	myCar.setColor("red");
+//
+//	cout << "차의 속도:" << myCar.getSpeed() << endl;
+//	cout << "차의 기어:" << myCar.getGear() << endl;
+//	cout << "차의 색상:" << myCar.getColor() << endl;
+//	return 0;
+//}
---
+++ Espresso5/car4.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Car {
+private:
+	int speed;
+	int gear;
+	string color;
+
+public:
+	int getSpeed();
+	void setSpeed(int s);
+	int getGear();
+	void setGear(int g);
+	string getColor();
+	void setColor(string c);
+
+
+};
+
+int Car::getSpeed()
+{
+	return speed;
+}
+
+void Car::setSpeed(int s)
+{
+	speed = s;
+}
+
+int Car::getGear()
+{
+	return gear;
+}
+
+void Car::setGear(int g)
+{
+	gear = g;
+}
+
+string Car::getColor()
+{
+	return color;
+}
+
+void Car::setColor(string c)
+{
+	color = c;
+}
+int main()
+{
+	Car myCar;
+	myCar.setSpeed(100);
+	myCar.setGear(3);
+	myCar.setColor("red");
+
+	cout << "차의 속도: " << myCar.getSpeed() << endl;
+	cout << "차의 기어: " << myCar.getGear() << endl;
+	cout << "차의 색상: " << myCar.getColor() << endl;
+	return 0;
+}
