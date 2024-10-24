b209cfbc54e1f26861ce1a1da251ddb4e2d3964f
heonjunkim <s01077314486@gmail.com>
2024-10-24 23:05:42 +0900

    car5

--- Espresso5/car.h
+++ Espresso5/car.h
@@ -1,14 +1,14 @@
-#include<iostream>
-#include<string>
-using namespace std;
-class Car
-{
-private:
-	int speed;
-	int gear;
-	string color;
-public:
-	int getSpeed();
-	void setSpeed(int s);
-};
-
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//class Car
+//{
+//private:
+//	int speed;
+//	int gear;
+//	string color;
+//public:
+//	int getSpeed();
+//	void setSpeed(int s);
+//};
+//
