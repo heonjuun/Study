a314eef7344156c078e39de4f587d8910af53e49
heonjunkim <s01077314486@gmail.com>
2024-10-28 22:27:04 +0900

    product

--- Espresso5/bank_account.cpp
+++ Espresso5/bank_account.cpp
@@ -1,52 +1,52 @@
-#include<iostream>
-#include<string>
-using namespace std;
-
-class BankAccount {
-private:
-	int accountNumber;
-	string owner;
-	int balance;
-public:
-	void setBalance(int amount);
-	int getBalance();
-	void deposit(int amount);
-	void withdraw(int amount);
-	void print();
-};
-void BankAccount::setBalance(int amount)
-{
-	balance = amount;
-}
-
-int BankAccount::getBalance()
-{
-	return balance;
-}
-
-void BankAccount::deposit(int amount)
-{
-	balance += amount;
-}
-
-void BankAccount::withdraw(int amount)
-{
-	balance -= amount;
-}
-
-void BankAccount::print()
-{
-	cout << "잔액은" << balance << "입니다." << endl;
-}
-int main()
-{
-	BankAccount account;
-	account.setBalance(0);
-	account.deposit(10000);
-	account.print();
-
-	account.withdraw(8000);
-	account.print();
-
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//class BankAccount {
+//private:
+//	int accountNumber;
+//	string owner;
+//	int balance;
+//public:
+//	void setBalance(int amount);
+//	int getBalance();
+//	void deposit(int amount);
+//	void withdraw(int amount);
+//	void print();
+//};
+//void BankAccount::setBalance(int amount)
+//{
+//	balance = amount;
+//}
+//
+//int BankAccount::getBalance()
+//{
+//	return balance;
+//}
+//
+//void BankAccount::deposit(int amount)
+//{
+//	balance += amount;
+//}
+//
+//void BankAccount::withdraw(int amount)
+//{
+//	balance -= amount;
+//}
+//
+//void BankAccount::print()
+//{
+//	cout << "잔액은" << balance << "입니다." << endl;
+//}
+//int main()
+//{
+//	BankAccount account;
+//	account.setBalance(0);
+//	account.deposit(10000);
+//	account.print();
+//
+//	account.withdraw(8000);
+//	account.print();
+//
+//	return 0;
+//}
