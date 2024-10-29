e833cf0d9cf450c97a227fb3fa65e9a46e3000e9
heonjunkim <s01077314486@gmail.com>
2024-10-29 22:15:44 +0900

    time

---
+++ Espresso5/Time.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Time {
+public:
+	int hour, minute;
+	void print();
+};
+
+void Time::print()
+{
+	cout << hour << ":" << minute << endl;
+}
+
+int main()
+{
+	Time t;
+	t.hour = 11;
+	t.minute = 35;
+	t.print();
+	return 0;
+}
