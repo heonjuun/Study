79da2b75a0e8a89d02f4ed377d55474826c5ccbb
heonjunkim <s01077314486@gmail.com>
2024-10-30 20:50:08 +0900

    book or rectangle

---
+++ Espresso5/Programming1.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//using namespace std;
+//
+//class Rectangle {
+//public:
+//	double width;
+//	double length;
+//	void calcArea();
+//};
+//
+//void Rectangle::calcArea()
+//{
+//	cout << "사각형의 넓이:" << width * length;
+//}
+//int main()
+//{
+//	Rectangle rec;
+//
+//	rec.length=50.1;
+//	rec.width = 20.1;
+//	rec.calcArea();
+//	return 0;
+//
+//}
---
+++ Espresso5/Programming2.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//class Book {
+//public:
+//	string title;
+//	string author;
+//	void bookprint();
+//};
+//
+//void Book::bookprint()
+//{
+//	cout << "제목: " << title<<endl;
+//	cout << "저자: " << author ;
+//}
+//int main()
+//{
+//	Book book;
+//	book.author = "한강";
+//	book.title = "채식 주의자";
+//	book.bookprint();
+//
+//	return 0;
+//}
