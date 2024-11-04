571305ef5532b10e755341ea921fb4e00738b3b9
heonjunkim <s01077314486@gmail.com>
2024-11-04 23:17:16 +0900

    car

---
+++ Espresso6/car1.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//class Car {
+//private:
+//	int speed;
+//	int gear;
+//	string color;
+//public:
+//	Car()
+//	{
+//		cout << "디폴트 생성자" << endl;
+//		speed = 0;
+//		gear = 1;
+//		color = "red";
+//
+//	}
+//};
+//int main()
+//{
+//	Car c1;
+//	return 0;
+//}
