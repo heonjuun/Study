cd856fa72bc44f1444c0bc171a53b21497f0e884
heonjunkim <s01077314486@gmail.com>
2024-11-16 23:03:40 +0900

    functionpointer

--- Espresso7/object_func1.cpp
+++ Espresso7/object_func1.cpp
@@ -1,31 +1,42 @@
-#include<iostream>
-#include<string>
-using namespace std;
-
-class Car {
-	int speed;
-	int gear;
-	string color;
-public:
-	Car(int s = 0, int g = 1, string c="white"):speed(s),gear(g),color(c){}
-	void print()
-	{
-		cout << "속도:" << speed << " 기어:" << gear << " 색상:" << color << endl;
-	}
-};
-void swapObjects(Car c1, Car c2)
-{
-	Car tmp;
-	tmp = c1;
-	c1 = c2;
-	c2 = tmp;
-}
-int main()
-{
-	Car red(0, 1, "red");
-	Car blue(30, 2, "blue");
-	swapObjects(red, blue);
-	red.print();
-	blue.print();
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//class Car {
+//	int speed;
+//	int gear;
+//	string color;
+//public:
+//	Car(int s = 0, int g = 1, string c="white"):speed(s),gear(g),color(c){}
+//	void print()
+//	{
+//		cout << "속도:" << speed << " 기어:" << gear << " 색상:" << color << endl;
+//	}
+//};
+//void swapObjects(Car c1, Car c2)
+//{
+//	Car tmp;
+//	tmp = c1;
+//	c1 = c2;
+//	c2 = tmp;
+//}
+//Car createCar()
+//{
+//	Car tmp(0, 1, "metal");
+//	return tmp;
+//}
+//int main()
+//{
+//	//Car red(0, 1, "red");
+//	//Car blue(30, 2, "blue");
+//	//swapObjects(red, blue);
+//	//red.print();
+//	//blue.print();
+//	Car c;
+//	c.print();
+//	c = createCar();
+//	c.print();
+//
+//
+//	return 0;
+//}
