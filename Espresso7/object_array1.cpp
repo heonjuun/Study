9da9d3f4cfc5d0739e4472379560d4447ecb5393
heonjunkim <s01077314486@gmail.com>
2024-11-22 21:07:08 +0900

    i love

--- Espresso7/object_array1.cpp
+++ Espresso7/object_array1.cpp
@@ -1,37 +1,37 @@
-#include<iostream>
-#include<string>
-using namespace std;
-class Car {
-	int speed;
-	int gear;
-	string color;
-public:
-	Car(int s = 0, int g = 0, string c = "white") :speed(s), gear(g), color(c) {}
-	void print();
-};
-
-void Car::print()
-{
-	cout << "속도:" << speed << "기어:" << gear << "색상:" << color<<endl;
-}
-int main()
-{
-	Car objArray[3] = {
-		Car(0,1,"white"),
-		Car(0,1,"red"),
-		Car(0,1,"blue"),
-	};
-	for (int i = 0; i < 3; i++)
-	{
-		(objArray + i)->print();
-	}
-	Car* p = objArray;
-	for (int i = 0; i < 3; i++)
-	{
-		p->print();
-		p++;
-	}
-
-	return 0;
-
-}
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//class Car {
+//	int speed;
+//	int gear;
+//	string color;
+//public:
+//	Car(int s = 0, int g = 0, string c = "white") :speed(s), gear(g), color(c) {}
+//	void print();
+//};
+//
+//void Car::print()
+//{
+//	cout << "속도:" << speed << "기어:" << gear << "색상:" << color<<endl;
+//}
+//int main()
+//{
+//	Car objArray[3] = {
+//		Car(0,1,"white"),
+//		Car(0,1,"red"),
+//		Car(0,1,"blue"),
+//	};
+//	for (int i = 0; i < 3; i++)
+//	{
+//		(objArray + i)->print();
+//	}
+//	Car* p = objArray;
+//	for (int i = 0; i < 3; i++)
+//	{
+//		p->print();
+//		p++;
+//	}
+//
+//	return 0;
+//
+//}
