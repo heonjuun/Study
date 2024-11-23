5f62709122d9d5cba696e6ae59ee8bfa0d978355
heonjunkim <s01077314486@gmail.com>
2024-11-23 16:43:08 +0900

    lab

--- Espresso7/alarm_clock.cpp
+++ Espresso7/alarm_clock.cpp
@@ -1,66 +1,66 @@
-#include<iostream>
-#include<string>
-using namespace std;
-class Time {
-private:
-	int time;
-	int minute;
-	int second;
-public:
-	Time();
-	Time(int t, int m, int s);
-	void print();
-};
-
-Time::Time()
-{
-	time = 0;
-	minute = 0;
-	second = 0;
-}
-
-Time::Time(int t, int m, int s)
-{
-	time = t;
-	minute = m;
-	second = s;
-}
-
-void Time::print()
-{
-	cout << time << "시" << minute << "분" << second << "초\n";
-}
-class AlarmClock
-{
-private:
-	Time currentTime;
-	Time alarmTime;
-public:
-	AlarmClock(Time a, Time c);
-	void print();
-};
-
-AlarmClock::AlarmClock(Time a, Time c)
-{
-	alarmTime = a;
-	currentTime = c;
-}
-
-void AlarmClock::print()
-{
-	cout << "현재시각:";
-	currentTime.print();
-	cout << "알람 시각:";
-	alarmTime.print();
-}
-int main()
-{
-	Time alarm(6, 0, 0);
-	Time current(12, 56, 34);
-	AlarmClock c(alarm, current);
-	c.print();
-
-	
-	
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//class Time {
+//private:
+//	int time;
+//	int minute;
+//	int second;
+//public:
+//	Time();
+//	Time(int t, int m, int s);
+//	void print();
+//};
+//
+//Time::Time()
+//{
+//	time = 0;
+//	minute = 0;
+//	second = 0;
+//}
+//
+//Time::Time(int t, int m, int s)
+//{
+//	time = t;
+//	minute = m;
+//	second = s;
+//}
+//
+//void Time::print()
+//{
+//	cout << time << "시" << minute << "분" << second << "초\n";
+//}
+//class AlarmClock
+//{
+//private:
+//	Time currentTime;
+//	Time alarmTime;
+//public:
+//	AlarmClock(Time a, Time c);
+//	void print();
+//};
+//
+//AlarmClock::AlarmClock(Time a, Time c)
+//{
+//	alarmTime = a;
+//	currentTime = c;
+//}
+//
+//void AlarmClock::print()
+//{
+//	cout << "현재시각:";
+//	currentTime.print();
+//	cout << "알람 시각:";
+//	alarmTime.print();
+//}
+//int main()
+//{
+//	Time alarm(6, 0, 0);
+//	Time current(12, 56, 34);
+//	AlarmClock c(alarm, current);
+//	c.print();
+//
+//	
+//	
+//	return 0;
+//}
---
+++ Espresso7/lab.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Student {
+private:
+	string name;
+	string telephone;
+};
