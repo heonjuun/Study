a1196322fed54832980bfdfd1918bd88acaa4ecb
heonjunkim <s01077314486@gmail.com>
2024-12-05 21:42:22 +0900

    sort

--- Espresso7/complex.cpp
+++ Espresso7/complex.cpp
@@ -1,76 +1,76 @@
-#include<iostream>
-using namespace std;
-
-class Complex
-{
-private:
-	double real;
-	double imag;
-public:
-	Complex();
-	Complex(double a, double b);
-	~Complex();
-
-	double getReal();
-	double getImag();
-	Complex add(const Complex& c);
-	void print();
-
-};
-
-Complex::Complex()
-{
-	real = 0;
-	imag = 0;
-}
-
-Complex::Complex(double a, double b)
-{
-	real = a;
-	imag = b;
-}
-
-Complex::~Complex()
-{
-}
-
-double Complex::getReal()
-{
-	return(real);
-}
-
-double Complex::getImag()
-{
-	return (imag);
-}
-
-Complex Complex::add(const Complex& c)
-{
-	Complex temp;
-	temp.real = this->real + c.real;
-	temp.imag = this->imag + c.imag;
-	return(temp);
-}
-
-void Complex::print()
-{
-	cout << real << "+" << imag << "i" << endl;
-}
-
-int main(void)
-{
-	Complex x(2, 3), y(4, 6), z;
-
-	cout << "첫 번째 복소수x:";
-	x.print();
-
-	cout << "두 번째 복소수x:";
-	y.print();
-
-	z = x.add(y);
-	cout << "z=x+y=";
-	z.print();
-	return(0);
-
-	
-}
+//#include<iostream>
+//using namespace std;
+//
+//class Complex
+//{
+//private:
+//	double real;
+//	double imag;
+//public:
+//	Complex();
+//	Complex(double a, double b);
+//	~Complex();
+//
+//	double getReal();
+//	double getImag();
+//	Complex add(const Complex& c);
+//	void print();
+//
+//};
+//
+//Complex::Complex()
+//{
+//	real = 0;
+//	imag = 0;
+//}
+//
+//Complex::Complex(double a, double b)
+//{
+//	real = a;
+//	imag = b;
+//}
+//
+//Complex::~Complex()
+//{
+//}
+//
+//double Complex::getReal()
+//{
+//	return(real);
+//}
+//
+//double Complex::getImag()
+//{
+//	return (imag);
+//}
+//
+//Complex Complex::add(const Complex& c)
+//{
+//	Complex temp;
+//	temp.real = this->real + c.real;
+//	temp.imag = this->imag + c.imag;
+//	return(temp);
+//}
+//
+//void Complex::print()
+//{
+//	cout << real << "+" << imag << "i" << endl;
+//}
+//
+//int main(void)
+//{
+//	Complex x(2, 3), y(4, 6), z;
+//
+//	cout << "첫 번째 복소수x:";
+//	x.print();
+//
+//	cout << "두 번째 복소수x:";
+//	y.print();
+//
+//	z = x.add(y);
+//	cout << "z=x+y=";
+//	z.print();
+//	return(0);
+//
+//	
+//}
