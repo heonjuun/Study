cd2658db40b38091952c4ca87925d896f761c107
heonjunkim <s01077314486@gmail.com>
2024-12-16 01:10:41 +0900

    borntodie

--- Espresso8/inheri_employee.cpp
+++ Espresso8/inheri_employee.cpp
@@ -1,51 +1,51 @@
-#include<iostream>
-#include<string>
-using namespace std;
-
-class Employee {
-private:
-	int rrn; //regident resgistration number
-protected:
-	int salary;
-public:
-	string name;
-	void setSalary(int salary);
-	int getSalary();
-};
-
-void Employee::setSalary(int salary)
-{
-	this->salary = salary;
-}
-
-int Employee::getSalary()
-{
-	return salary;
-}
-class Manager :public Employee {
-	int bonus;
-public:
-	Manager(int b = 0) : bonus(b) {}
-	void modify(int s, int b);
-	void display();
-};
-
-void Manager::modify(int s, int b)
-{
-	salary = s;
-	bonus = b;
-}
-
-void Manager::display()
-{
-	cout << "ºÀ±Þ:" << salary << " º¸³Ê½º:" << bonus << endl;
-
-}
-int main()
-{
-	Manager m;
-	m.setSalary(2000);
-	m.display();
-	m.modify(1000, 500);
-	m.display();
-}
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//class Employee {
+//private:
+//	int rrn; //regident resgistration number
+//protected:
+//	int salary;
+//public:
+//	string name;
+//	void setSalary(int salary);
+//	int getSalary();
+//};
+//
+//void Employee::setSalary(int salary)
+//{
+//	this->salary = salary;
+//}
+//
+//int Employee::getSalary()
+//{
+//	return salary;
+//}
+//class Manager :public Employee {
+//	int bonus;
+//public:
+//	Manager(int b = 0) : bonus(b) {}
+//	void modify(int s, int b);
+//	void display();
+//};
+//
+//void Manager::modify(int s, int b)
+//{
+//	salary = s;
+//	bonus = b;
+//}
+//
+//void Manager::display()
+//{
+//	cout << "ºÀ±Þ:" << salary << " º¸³Ê½º:" << bonus << endl;
+//
+//}
+//int main()
+//{
+//	Manager m;
+//	m.setSalary(2000);
+//	m.display();
+//	m.modify(1000, 500);
+//	m.display();
+//}
