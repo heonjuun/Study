587544edf6769183d524ce04ef74cc64c4e828c9
heonjunkim <s01077314486@gmail.com>
2025-02-04 23:17:17 +0900

    1

---
+++ Espresso8/inheri_car2.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//class Car {
+//public:
+//	int getHP()
+//	{
+//		return 100;
+//	}
+//};
+//class SportsCar :public Car {
+//public:
+//	int getPH()
+//	{
+//		return 300;
+//	}
+//};
+//int main()
+//{
+//	SportsCar sc;
+//	cout << "¸¶·Â : " << sc.getHP() << endl;
+//	return 0;
+//}
