1410684d7bd9b4bb016be507e9d147302d103fa3
heonjunkim <s01077314486@gmail.com>
2025-02-12 19:02:21 +0900

    inherit

---
+++ Espresso8/inheri_child2.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class ParentClass {
+private:
+	int x;
+protected:
+	int y;
+public:
+	int z;
+};
+
+class ChildClass1 : public ParentClass
+{};
+class ChildClass2 : protected ParentClass
+{};
+class ChildClass3 : private ParentClass
+{};
+
+int main()
+{
+	ChildClass1 obj1;
+	ChildClass2 obj2;
+	ChildClass3 obj3;
+
+	cout << obj1.x << endl;
+	cout << obj1.y << endl;
+	cout << obj1.z << endl;
+
+	cout << obj2.x << endl;
+	cout << obj2.y << endl;
+	cout << obj2.z << endl;
+
+	cout << obj3.x << endl;
+	cout << obj3.y << endl;
+	cout << obj3.z << endl;
+
+}
