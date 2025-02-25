22108703ff70fb4410d06fde580e93295aa07174
heonjunkim <s01077314486@gmail.com>
2025-02-25 23:40:16 +0900

    multiinherit2

---
+++ Espresso8/dog.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+enum BREED{YORKIE, POODLE, BULLDOG };
+
+class Animal
+{
+protected:
+	int age;
+	int weight;
+
+};
---
+++ Espresso8/inheri_multi2.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//using namespace std;
+//
+//
+//class SuperA
+//{
+//public:
+//	int x;
+//	void sub()
+//	{
+//		cout << "SuperAÀÇ sub()" << endl;
+//	}
+//};
+//class SuperB
+//{
+//public:
+//	int x;
+//	void sub()
+//	{
+//		cout << "SuperBÀÇ sub()" << endl;
+//	}
+//};
+//class Sub : public SuperA, public SuperB
+//{
+//
+//};
+//int main()
+//{
+//	Sub obj;
+//	obj.SuperA::x = 10;
+//	return 0;
+//}
