fe36a7aade7a6f86f12c4ce383a949c143dd4d49
heonjunkim <s01077314486@gmail.com>
2025-02-27 22:48:35 +0900

    inherit_display

--- Espresso8/dog.cpp
+++ Espresso8/dog.cpp
@@ -1,89 +1,89 @@
-#include<iostream>
-using namespace std;
-enum BREED{YORKIE, POODLE, BULLDOG };
-
-class Animal
-{
-protected:
-	int age;
-	int weight;
-public:
-	Animal();
-	~Animal();
-
-	void speak() const;
-	void sleep() const;
-	void eat() const;
-};
-
-Animal::Animal()
-{
-	cout << "Animal 持失切\n";
-}
-
-Animal::~Animal()
-{
-	cout << "Animal 社瑚切\n";
-}
-
-void Animal::speak() const
-{
-	cout << "Animal speak()\n";
-}
-
-void Animal::sleep() const
-{
-	cout << "Animal sleep()\n";
-}
-
-void Animal::eat() const
-{
-	cout << "Animal eat()\n";
-}
-class Dog : public Animal
-{
-private:
-	BREED breed;
-public:
-	Dog();
-	~Dog();
-
-	void wag();
-	void bite();
-	void speak() const;
-};
-
-Dog::Dog()
-{
-	cout << "Dog 持失切\n";
-}
-
-Dog::~Dog()
-{
-	cout << "Dog 社瑚切\n";
-}
-
-void Dog::wag()
-{
-	cout << "Dog Wag()\n";
-}
-
-void Dog::bite()
-{
-	cout << "Dog Bite()\n";
-}
-
-void Dog::speak() const
-{
-	cout << "Dog Speak()\n";
-}
-int main()
-{
-	Dog dog;
-	dog.eat();
-	dog.sleep();
-	dog.speak();
-	dog.wag();
-	return 0;
-
-}
+//#include<iostream>
+//using namespace std;
+//enum BREED{YORKIE, POODLE, BULLDOG };
+//
+//class Animal
+//{
+//protected:
+//	int age;
+//	int weight;
+//public:
+//	Animal();
+//	~Animal();
+//
+//	void speak() const;
+//	void sleep() const;
+//	void eat() const;
+//};
+//
+//Animal::Animal()
+//{
+//	cout << "Animal 持失切\n";
+//}
+//
+//Animal::~Animal()
+//{
+//	cout << "Animal 社瑚切\n";
+//}
+//
+//void Animal::speak() const
+//{
+//	cout << "Animal speak()\n";
+//}
+//
+//void Animal::sleep() const
+//{
+//	cout << "Animal sleep()\n";
+//}
+//
+//void Animal::eat() const
+//{
+//	cout << "Animal eat()\n";
+//}
+//class Dog : public Animal
+//{
+//private:
+//	BREED breed;
+//public:
+//	Dog();
+//	~Dog();
+//
+//	void wag();
+//	void bite();
+//	void speak() const;
+//};
+//
+//Dog::Dog()
+//{
+//	cout << "Dog 持失切\n";
+//}
+//
+//Dog::~Dog()
+//{
+//	cout << "Dog 社瑚切\n";
+//}
+//
+//void Dog::wag()
+//{
+//	cout << "Dog Wag()\n";
+//}
+//
+//void Dog::bite()
+//{
+//	cout << "Dog Bite()\n";
+//}
+//
+//void Dog::speak() const
+//{
+//	cout << "Dog Speak()\n";
+//}
+//int main()
+//{
+//	Dog dog;
+//	dog.eat();
+//	dog.sleep();
+//	dog.speak();
+//	dog.wag();
+//	return 0;
+//
+//}
---
+++ Espresso8/inheri_employee2.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Person {
+	string name;
+	int age;
+	bool gender;
+public:
+	Person(string n="", int a=0, bool g =true):name(n),age(a),gender(g){}
+	void setName(string s) { name = s; }
+	string getName() const { return name; }
+	void setAge(int a) { age = a; }
+	int getAge()const { return age; }
+	void setGender(bool g) { gender = g; }
+	bool getGender()const { return gender; }
+};
+class Employee :public Person {
+	int number;
+	int salary;
+public:
+	Employee(string n = "", int a = 0, bool g = true, int num = 0, int s = 0) :
+		Person(n, a, g), number(num), salary(s) {}
+	void display() const;
+	void setNumber(int n) { number = n; }
+	int getNumber()const { return number; }
+	void setSalary(int s) { salary = s; }
+	int getSalary()const { return salary; }
+};
+void Employee::display()const
+{
+	cout << this->getName() << endl;
+	cout << this->getAge() << endl;
+	cout << this->getGender() << endl;
+	cout << this->getNumber() << endl;
+	cout << this->getSalary() << endl;
+}
+int main()
+{
+	Employee e("±èÃ¶", 25, true, 2205801, 3000);
+	e.display();
+	return 0;
+}
