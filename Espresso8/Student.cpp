fdc6ffdcf1154a02123cf86ad7ca48d09306d1af
heonjunkim <s01077314486@gmail.com>
2025-04-28 21:22:29 +0900

    syu

---
+++ Espresso8/Student.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//class Student {
+//	int number;
+//public:
+//	string name;
+//};
+//class GraduateStudent : public Student {
+//public:
+//	string lab;	
+//};
+//int main()
+//{
+//	GraduateStudent s;
+//	s.lab = "LAB";
+//	s.name = "ȫ�浿";
+//
+//	return 0;
+//}
